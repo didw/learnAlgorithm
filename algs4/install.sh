f5f6e8873b50356af578f939e72d0dc7331109d7
Jongyeol Yang <yangjy0113@gmail.com>
2016-02-10 13:32:01 +0900

    add algs4.jar, and add script to install checklist, findbug

---
+++ algs4/install.sh
@@ -0,0 +1,8 @@
+# install algs4.jar
+wget http://algs4.cs.princeton.edu/code/algs4.jar
+
+# install checkstyle
+http://eclipse-cs.sourceforge.net/#!/
+
+# install findbugs
+http://findbugs.cs.umd.edu/eclipse/
