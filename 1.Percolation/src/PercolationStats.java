f0bdf5e8b3cfea39dc3171321bb7586c3654439f
Jongyeol Yang <yangjy0113@gmail.com>
2016-02-11 22:19:25 +0900

    100 score

--- 1.Percolation/src/PercolationStats.java
+++ 1.Percolation/src/PercolationStats.java
@@ -6,16 +6,17 @@ public class PercolationStats {
    private int t;
    public PercolationStats(int N, int T)     // perform T independent experiments on an N-by-N grid
    {
+      if (N <= 0 || T <= 0) throw new IllegalArgumentException("N or T is illegal");
       t = T;
       thred = new double[T];
+      Random r1 = new Random();
       for (int i = 0; i < T; ++i) {
          thred[i] = 0;
          Percolation proc = new Percolation(N);
-         Random r1 = new Random(0);
          while (!proc.percolates()) {
-            int y = r1.nextInt(N-1);
-            int x = r1.nextInt(N-1);
-            if (!proc.isOpen(y, x)) {
+            int y = r1.nextInt(N);
+            int x = r1.nextInt(N);
+            if (!proc.isOpen(y+1, x+1)) {
                proc.open(y+1, x+1);
                thred[i] += 1;
             }
@@ -36,17 +37,20 @@ public class PercolationStats {
    
    public double confidenceLo()              // low  endpoint of 95% confidence interval
    {
-      return StdStats.mean(thred) - 1.96 * StdStats.stddev(thred)/t;
+      return StdStats.mean(thred) - 1.96 * StdStats.stddev(thred)/Math.sqrt(t);
    }
    
    public double confidenceHi()              // high endpoint of 95% confidence interval
    {
-      return StdStats.mean(thred) + 1.96 * StdStats.stddev(thred)/t;      
+      return StdStats.mean(thred) + 1.96 * StdStats.stddev(thred)/Math.sqrt(t);      
    }
 
-   public static void main(String[] args)    // test client (described below)
-   {
-      
-   }
 
+   public static void main(String[] args) {
+      PercolationStats pes = new PercolationStats(100, 10);
+      System.out.println(pes.mean());
+      System.out.println(pes.stddev());
+      System.out.println(pes.confidenceLo());
+      System.out.println(pes.confidenceHi());
+   }
 }
