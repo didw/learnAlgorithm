82002ca8b7faa6cca27b19f41e7f4f14c1a7617a
Jongyeol Yang <yangjy0113@gmail.com>
2016-02-23 23:29:52 +0900

    second assignment: starter code

---
+++ 2.RandomizedQueuesAndDeques/src/Deque.java
@@ -0,0 +1,84 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+   private Item[] arr;
+   private int left, right, N;
+   public Deque() {                           // construct an empty deque
+      arr = (Item[])new Object[4];
+      left = 2;
+      right = 2;
+      N = 4;
+   }
+   public boolean isEmpty() {                // is the deque empty?
+      return left == right;
+   }
+   public int size() {                        // return the number of items on the deque
+      return right - left;
+   }
+   private void stretchLeft() {
+      Item[] arr_new = (Item[])new Object[N*2];
+      for (int i = 0; i < N; ++i) {
+         arr_new[i+N] = arr[i];
+      }
+      arr = arr_new;
+      right += N;
+      left += N;
+      N *= 2;
+   }
+   public void addFirst(Item item) {          // add the item to the front
+      arr[left--] = item;
+      if (left == 0) stretchLeft();
+   }
+   private void stretchRight() {
+      Item[] arr_new = (Item[])new Object[N*2];
+      for (int i = 0; i < N; ++i)
+         arr_new[i] = arr[i];
+      arr = arr_new;
+      N *= 2;
+   }
+   public void addLast(Item item) {           // add the item to the end
+      arr[right++] = item;
+      if (right == N-1) stretchRight();
+   }
+   private void shortenLeft() {
+      N /= 2;
+      Item[] arr_new = (Item[])new Object[N];
+      for (int i = 0; i < N; ++i) {
+         arr_new[i] = arr[i+N];
+      }
+      arr = arr_new;
+      left -= N;
+      right -= N;
+   }
+   public Item removeFirst() {               // remove and return the item from the front
+      if (isEmpty()) return null;
+      Item ni = arr[left++];
+      if (N > 4 && left > N*3/4) shortenLeft();
+      return ni;
+   }
+   private void shortenRight() {
+      N /= 2;
+      Item[] arr_new = (Item[])new Object[N];
+      for (int i = 0; i < N; ++i) {
+         arr_new[i] = arr[i];
+      }
+      arr = arr_new;
+   }
+   public Item removeLast() {                // remove and return the item from the end
+      if (isEmpty()) return null;
+      Item ni = arr[right--];
+      if (N>4 && right < N/4) shortenRight();
+      return ni;
+   }
+   @Override
+   public Iterator<Item> iterator() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+
+   }
+
+}
---
+++ 2.RandomizedQueuesAndDeques/src/RandomizedQueue.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+   public RandomizedQueue() {                // construct an empty randomized queue
+      
+   }
+   public boolean isEmpty() {                // is the queue empty?
+      
+   }
+   public int size() {                       // return the number of items on the queue
+      
+   }
+   public void enqueue(Item item) {          // add the item
+      
+   }
+   public Item dequeue() {                   // remove and return a random item
+      
+   }
+   public Item sample() {                    // return (but do not remove) a random item
+      
+   }
+   @Override
+   public Iterator<Item> iterator() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+
+   }
+
+}
