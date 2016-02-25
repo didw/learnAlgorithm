4e7a730d3899184b0ece1d7da6d4b5a4a1250ef1
Jongyeol Yang <yangjy0113@gmail.com>
2016-02-25 22:28:35 +0900

    bug fixed

--- 2.RandomizedQueuesAndDeques/src/RandomizedQueue.java
+++ 2.RandomizedQueuesAndDeques/src/RandomizedQueue.java
@@ -42,7 +42,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
    private void decreaseSize() {
       N /= 2;
       Item[] new_arr = (Item[])new Object[N];
-      for (int i = 0; i < N/2; --i)
+      for (int i = 0; i < N/2; ++i)
          new_arr[i] = arr[i];
       arr = new_arr;
    }
